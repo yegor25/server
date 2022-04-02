680462aee81489bfeedd46d5e63f120222b22c42
yegor25 <lesnichij94@mail.ru>
2022-04-02 21:20:12 +0300

    second commit

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express()
 
 const mongoose = require('mongoose')
+const MONGO_USERNAME = 'admin';
+const MONGO_PASSWORD = 'admin';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'MyDb';
 const { response } = require('express')
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 app.use(express.json())
 
 
 
 const start = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/MyDb")
+        await mongoose.connect(url)
         .then( () => console.log('mongo connection'))
         app.listen(3001, () => {
             console.log('server started');
